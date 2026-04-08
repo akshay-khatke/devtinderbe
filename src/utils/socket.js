3c25146d541dc7d23178f152ee906c6a76c3711a
akshay-khatke <khatkeakki@gmail.com>
2026-04-08 12:39:49 +0530

    changes of localhost

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -12,6 +12,8 @@ export const socketConnection = (serverConnection) => {
         cors: {
             origin: (origin, callback) => {
                 const allowedOrigins = [
+                    "http://localhost:5174",
+                    "http://127.0.0.1:5174",
                     "http://localhost:5173",
                     "http://localhost:3000",
                     "http://localhost:10000", // The user's backend port
