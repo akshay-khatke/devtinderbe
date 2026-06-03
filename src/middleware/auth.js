44962078e753b1aca6ace3eeca46b576b86f2be6
akshay-khatke <khatkeakki@gmail.com>
2026-06-03 18:21:13 +0530

    change of socket for mobile

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,13 +3,20 @@ import User from "../model/user.js";
 
 export const userAuth = async (req, res, next) => {
     try {
-        const token = req.cookies.token
-        // console.log(req.cookies, 'check the token in middleware')
+        let token = req.cookies.token;
+        
+        if (!token && req.headers.authorization) {
+            if (req.headers.authorization.startsWith("Bearer ")) {
+                token = req.headers.authorization.split(" ")[1];
+            } else {
+                token = req.headers.authorization;
+            }
+        }
+
         if (!token) {
             return res.status(401).send("invalid token")
         }
         const isTokenValid = await jwt.verify(token, process.env.JWT_SECRETE_KEY)
-        // console.log(isTokenValid._id, 'check the token VERIFY')
         const user = await User.findById(isTokenValid._id)
         if (!user) {
             return res.status(400).send("user does not found")
@@ -19,7 +26,7 @@ export const userAuth = async (req, res, next) => {
     }
     catch (err) {
         console.log(err, "check the error")
-        res.status(400).send("ERROR :", err,)
+        res.status(401).send("invalid token")
     }
 
 }
